85746b5111c87d27d6f93ea8c1736b0ca1e5ec39
James Anderson <james.r.andreson@utah.edu>
2013-09-09 14:57:33 -0700

    Fix for recursive test entry by testing for '__main__'

--- setup.py
+++ setup.py
@@ -1,78 +1,59 @@
-#from distutils.core import setup
+'''
+Created on Aug 30, 2013
+
+@author: James Anderson
+'''
+
+from ez_setup import use_setuptools
+from setuptools import setup, find_packages
+
 
 import sys
-# from cx_Freeze import setup, Executable
-from distutils.core import setup
 
-if not hasattr(sys, 'frozen'):
-    import wxversion
-    wxversion.select('2.8')
-import wx
+if __name__ == '__main__':
+    use_setuptools()
 
-# import py2exe
-# import PyreGui
-import matplotlib
-from glob import glob
-import scipy
+    data_files = []
+    # data_files = {'pyre' : ['pyre/resources/*.png']}
+    # data_files = [("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
+    # data_files=matplotlib.get_py2exe_datafiles()
+    # data_files.append(("Microsoft.VC90.CRT", ['msvcp90.dll']))
+    # data_files.append(("", glob(r'*.png')))
 
-data_files = []
-# data_files = {'pyre' : ['pyre/resources/*.png']}
-#data_files = [("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]     
-# data_files=matplotlib.get_py2exe_datafiles()
-# data_files.append(("Microsoft.VC90.CRT", ['msvcp90.dll']))
-# data_files.append(("", glob(r'*.png')))
+    required_packages = ["numpy",
+                         "scipy",
+                         "matplotlib",
+                         "pyglet",
+                         "nornir_pools",
+                         "nornir_shared",
+                         "nornir_imageregistration",
+                         "wx",
+                         "wxversion"]
+
+    dependency_links = ["git+http://github.com/jamesra/nornir-pools#egg=nornir_pools",
+                        "git+http://github.com/jamesra/nornir-shared#egg=nornir_shared",
+                        "git+http://github.com/jamesra/nornir-imageregistration#egg=nornir_imageregistration"]
 
 
-excludes = ["Tkconstants","Tkinter","tcl",'_gtkagg', '_tkagg']
-includes = [
-                r'matplotlib',
-                r'numpy',
-                r'scipy',
-                r'pyglet',
-                 'wxversion',
-                r'wx',
-                 'scipy.special._cephes',
-                 'scipy.special.orthogonal_eval',
-                 'scipy.special._logit',
-                 'scipy.special.add_newdocs',
-                 'scipy.linalg.fblas',
-                 'scipy.linalg.flapack',
-                 'scipy.linalg.clapack',
-                 'scipy.linalg.cblas',
-                 'scipy.sparse.csgraph',
-                 'scipy.sparse.csgraph._validation',
-                 'nornir_imageregistration',
-                 'nornir_shared',
-                 'nornir_pools',
-                 'sys']
 
-includes = []
-packages = ['pyre']
+    includes = []
+    packages = ['pyre']
 
-build_exe_options = {"packages": packages,
-                     "excludes": excludes,
-                     "includes" : includes}
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+    # GUI applications require a different base on Windows (the default is for a
+    # console application).
+    base = None
+    if sys.platform == "win32":
+        base = "Win32GUI"
 
-#setup(data_files=data_files, console=['Pyre.py'])
-setup(name="pyre",
-      version="1.0.0",
-      data_files=data_files,
-      description='Python Image Registration Tool',
-      author='James Anderson and Drew Ferrell',
-      author_email='james.r.anderson@utah.edu',
-      console=['pyre.py'],
-      requires=includes,
-      packages=packages,
-      package_data={'pyre' : ['resources/*.png']},
-      options={
-                'py2exe': {'excludes': excludes,
-                           'includes': includes},
-                "build_exe" : build_exe_options})
-
-#      executables = [Executable("Pyre.py", base=base)])
+    # setup(data_files=data_files, console=['Pyre.py'])
+    setup(name="pyre",
+          version="1.0.0",
+          data_files=data_files,
+          description='Python Image Registration Tool',
+          author='James Anderson and Drew Ferrell',
+          author_email='james.r.anderson@utah.edu',
+          console=['pyre.py'],
+          required_packages=required_packages,
+          dependency_links=dependency_links,
+          package_data={'pyre' : ['resources/*.png']})
