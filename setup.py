46c4bf1ca6d83508062ffa817f84114d09dc2ff5
James Anderson <james.r.andreson@utah.edu>
2017-04-19 11:03:26 -0700

    Version Bump

--- setup.py
+++ setup.py
@@ -19,20 +19,23 @@ if __name__ == '__main__':
     # data_files=matplotlib.get_py2exe_datafiles()
     # data_files.append(("Microsoft.VC90.CRT", ['msvcp90.dll']))
     # data_files.append(("", glob(r'*.png')))
+    
+    
+    #OK to use pools v1.3.1, no changes made for v1.3.2
 
     required_packages = ["numpy>=1.8",
                          "scipy>=0.13.2",
                          "matplotlib",
                          "pyglet",
                          "nornir_pools>=1.3.1",
-                         "nornir_shared>=1.3.1",
-                         "nornir_imageregistration>=1.3.1",
+                         "nornir_shared>=1.3.2",
+                         "nornir_imageregistration>=1.3.2",
                          "PyOpenGL>=3.0",
                          "pillow>=2.3"]
 
     dependency_links = ["git+http://github.com/jamesra/nornir-pools#egg=nornir_pools-1.3.1",
-                        "git+http://github.com/jamesra/nornir-shared#egg=nornir_shared-1.3.1",
-                        "git+http://github.com/jamesra/nornir-imageregistration#egg=nornir_imageregistration-1.3.1"]
+                        "git+http://github.com/jamesra/nornir-shared#egg=nornir_shared-1.3.2",
+                        "git+http://github.com/jamesra/nornir-imageregistration#egg=nornir_imageregistration-1.3.2"]
 
 
 
@@ -49,7 +52,7 @@ if __name__ == '__main__':
 
     # setup(data_files=data_files, console=['Pyre.py'])
     setup(name="pyre",
-          version="1.3.1",
+          version="1.3.2",
           scripts=['scripts/start_pyre.py'],
           entry_points=entry_points,
           data_files=data_files,
